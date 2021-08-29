5e2d0007f3946e6e56fa6d91b89b5a6dbfadf862
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-08-29 10:45:43 +0300

    Created Imported Random Function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+import random
 
 kivy.require("1.9.0")
 
@@ -10,7 +11,8 @@ class MyRoot(BoxLayout):
     def __init__(self):
         super(MyRoot, self).__init__()
 
-    def generate_number (self):
+    def generate_number(self):
+        self.random_label.text = str(random.randint(0, 1000))
 
 
 class Neural_Random(App):
